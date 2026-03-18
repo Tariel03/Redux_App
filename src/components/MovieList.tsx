008acdc545d84d07d3337066b72880d926696253
Tariel03 <tariel.akmatov@gmail.com>
2026-03-18 08:49:30 +0100

    added remove option

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,14 +1,20 @@
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 import type {RootState} from "../store.ts";
+import {removeMovie} from "../movieSlice.ts";
 
 export default function MovieList() {
     const movies = useSelector((state: RootState) => state.movies.movies)
+    const dispatch = useDispatch();
+    const deleteMovie = (id: number) => {
+        dispatch(removeMovie(id));
+    }
     return (
         <div>
             <h1>Movie List!</h1>
             {movies.map((movie) => (
                 <div key={movie.id}>
                     <p>{movie.title}</p>
+                    <button onClick={ () => deleteMovie(movie.id)}> Delete movie!</button>
                 </div>
             ))}
 
